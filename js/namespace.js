454d7dc1b980bde48f7baf4c75531f0d309f4f52
Dan Stocker <dan@kwaia.com>
2014-10-03 07:22:25 +0100

    improves namespace documentation

--- js/namespace.js
+++ js/namespace.js
@@ -4,9 +4,70 @@
 /*global require */
 /** @namespace */
 var shoeshine = {},
-    s$ = shoeshine,
-    dessert = dessert || require('dessert'),
-    troop = troop || require('troop'),
-    sntls = sntls || require('sntls'),
-    evan = evan || require('evan'),
-    jQuery = jQuery || require('jquery');
+    s$ = shoeshine;
+
+/**
+ * @class
+ * @see https://github.com/production-minds/dessert
+ */
+var dessert = dessert || require('dessert');
+
+/**
+ * @namespace
+ * @see https://github.com/production-minds/troop
+ */
+var troop = troop || require('troop');
+
+/**
+ * @namespace
+ * @see https://github.com/danstocker/sntls
+ */
+var sntls = sntls || require('sntls');
+
+/**
+ * @namespace
+ * @see https://github.com/danstocker/evan
+ */
+var evan = evan || require('evan');
+
+
+/**
+ * @function
+ * @see http://api.jquery.com
+ */
+var jQuery = jQuery || require('jquery');
+
+/**
+ * Native number class.
+ * @name Number
+ * @class
+ */
+
+/**
+ * Native string class.
+ * @name String
+ * @class
+ */
+
+/**
+ * Native array class.
+ * @name Array
+ * @class
+ */
+
+/**
+ * Native DOM element class.
+ * @name Element
+ * @class
+ */
+
+/**
+ * Native DOM event class.
+ * @name Event
+ * @class
+ */
+
+/**
+ * @name sntls.Hash
+ * @class
+ */
